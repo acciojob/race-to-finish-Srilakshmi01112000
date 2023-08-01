4dc352ba509dab8711362b5405ac0fd1311d423c
Sri Lakshmi Iyengar K R <110050350+Srilakshmi01112000@users.noreply.github.com>
2023-08-01 11:35:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+function getRandomTime() {
+  return Math.floor(Math.random() * 5000) + 1000; // Random time between 1 and 5 seconds
+}
+
+const promises = [
+  new Promise(resolve => setTimeout(() => resolve(1), getRandomTime())),
+  new Promise(resolve => setTimeout(() => resolve(2), getRandomTime())),
+  new Promise(resolve => setTimeout(() => resolve(3), getRandomTime())),
+  new Promise(resolve => setTimeout(() => resolve(4), getRandomTime())),
+  new Promise(resolve => setTimeout(() => resolve(5), getRandomTime())),
+];
+
+Promise.any(promises)
+  .then(result => {
+    const outputDiv = document.getElementById('output');
+    outputDiv.textContent = `The first resolved promise: ${result}`;
+  })
+  .catch(error => {
+    console.error('Error occurred:', error);
+  });
